acd28092540451e530fae1a10eec87bf4c9441c7
Elena Ozerova <ozerova.lenka@gmail.com>
2023-07-29 12:35:21 +0200

    feat: remove text from footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,16 +7,13 @@ const Footer = () => {
       <div className='container'>
         <div className='footer_container'>
           <div>
-            <h2>Burger Shop</h2>
-
             <div className='footer_text'>
-              <p>We are trying to give you the best taste possible.</p>
-              <strong>All rights reserved by @burgershop &#9400; {new Date().getFullYear()}</strong>
+              <p>All rights reserved by @burgershop &#9400; {new Date().getFullYear()}</p>
             </div>
           </div>
 
           <aside>
-            <h4>Follow Us</h4>
+            <p>Follow Us</p>
             <div className='aside-links'>
               <a href='./'>
                 <AiFillYoutube />
